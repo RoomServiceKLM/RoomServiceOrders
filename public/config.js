e2934fe52a42a55f9488d0ff48a6e775efcc91b0
RoomServiceKLM <roomservicekimptonlosmonteros@gmail.com>
2026-08-11 00:31:05 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // Pega aquí SOLO la URL /exec de tu Apps Script.
 // Debe empezar por https://script.google.com/macros/s/ y terminar en /exec
-window.ROOM_SERVICE_WEBAPP_URL = 'PEGA_AQUI_TU_URL_EXEC';
+window.ROOM_SERVICE_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzM80N9D4J9Trg--SkZMSZIekjJLvElvzpjlZYH4ktUV_yv3lykL9Pfu75S0E8j-payMg/exec';
 
 // Deja esto vacío. Las habitaciones bloqueadas se ponen en la pestaña NoPost de Google Sheets.
 window.ROOM_SERVICE_NO_POST = [];
